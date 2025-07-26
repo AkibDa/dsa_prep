f32535760a8bb61c522729f1245bd6f63fb9466f
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-07-26 20:43:30 +0530

    checking if a number is armstrong or not

--- Java_Programs/practice/src/Number.java
+++ Java_Programs/practice/src/Number.java
@@ -1,5 +1,35 @@
+import java.lang.Math;
 import java.util.Scanner;
+
 public class Number {
+
+    public static int count_digits(int n){
+        int count = 0;
+        while(n>0){
+            int digit = n%10;
+            count++;
+            n=n/10;
+        }
+        return count;
+    }
+
+    public static void armstrong_number(int n){
+        int m=n;
+        int digits = count_digits(n);
+        int digit,sum = 0;
+        while(n>0){
+            digit=n%10;
+            sum+= Math.pow(digit,digits);
+            n=n/10;
+        }
+        if(sum==m){
+            System.out.println(m+" is a armstrong number\n");
+        }
+        else{
+            System.out.println(m+" is not a armstrong number\n");
+        }
+    }
+
     public static void prime_number(int n){
         int count = 0;
         for(int i=1;i<=n;i++){
@@ -8,10 +38,10 @@ public class Number {
             }
         }
         if(count==2){
-            System.out.print(n+" is a Prime Number");
+            System.out.print(n+" is a Prime Number\n");
         }
         else{
-            System.out.print(n+" is not a Prime Number");
+            System.out.print(n+" is not a Prime Number\n");
         }
     }
 
@@ -20,5 +50,6 @@ public class Number {
         System.out.println("Enter a number: ");
         int num = sc.nextInt();
         prime_number(num);
+        armstrong_number(num);
     }
 }
