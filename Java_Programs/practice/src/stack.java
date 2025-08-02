f5e48f48dc53eb15a08d3e3d16a1536b61afe643
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-08-02 18:50:39 +0530

    made a push fuunction

--- Java_Programs/practice/src/stack.java
+++ Java_Programs/practice/src/stack.java
@@ -1,2 +1,23 @@
 public class stack {
+    int maxSize;
+    int[] stack;
+    int top;
+
+    public stack() {
+        maxSize = 5;
+        stack = new int[maxSize];
+        top = -1;
+    }
+
+    public void push(int data) {
+        if (top == maxSize - 1) {
+            System.out.println("Overflow");
+        }
+        else {
+            top++;
+            stack[top] = data;
+            System.out.println("Pushed: " + data);
+        }
+    }
+
 }
