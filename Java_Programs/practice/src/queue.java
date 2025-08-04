8f70910adfa16c817fca6cbe0b6a54e6f759077f
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-08-04 19:16:28 +0530

    implementing queue data structure

---
+++ Java_Programs/practice/src/queue.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+
+public class queue {
+
+    int MAX_SIZE;
+    int[] queue;
+    int front, rear;
+
+    public queue() {
+        MAX_SIZE = 10;
+        queue = new int[MAX_SIZE];
+        front = rear = -1;
+    }
+
+    public static void  main(String[] args) {
+        queue obj = new queue();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size of the queue:\n");
+        int size = sc.nextInt();
+        int choice;
+
+        while (true) {
+            System.out.println("\nEnter your choice:");
+            System.out.println("1. Enqueue");
+            System.out.println("2. Dequeue");
+            System.out.println("3. Display");
+            System.out.println("4. Exit");
+            choice = sc.nextInt();
+
+            switch (choice) {
+                case 1:
+                    System.out.println("Enter the number to push:");
+                    int number = sc.nextInt();
+                    obj.enqueue(number);
+                    break;
+                case 2:
+                    obj.dequeue();
+                    break;
+                case 3:
+                    obj.display();
+                    break;
+                case 4:
+                    System.out.println("Exiting program.");
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+
+}
