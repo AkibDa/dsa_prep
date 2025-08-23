c270aaf69ce05504c2d2d42af89abd7223717294
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-08-23 20:04:16 +0530

    printing a half inverted pyramid with numbers

--- apna_college/untitled/src/Patterns.java
+++ apna_college/untitled/src/Patterns.java
@@ -77,8 +77,8 @@ public class Patterns {
     }
 
     public static void half_inverted_pyramid_with_numbers(int n) {
-        for (int i = n; i >= 1; i--) {
-            for (int j = 1; j <= i; j++) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n-i+1; j++) {
                 System.out.print(j+" ");
             }
             System.out.println();
