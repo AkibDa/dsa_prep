856d6df7c7b4f3587fc257df1a2926be769c8bd7
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-08-23 20:07:50 +0530

    printing a floyd's triangle

--- apna_college/untitled/src/Patterns.java
+++ apna_college/untitled/src/Patterns.java
@@ -85,6 +85,17 @@ public class Patterns {
         }
     }
 
+    public static void floyd_triangle(int n) {
+        int k = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(k+" ");
+                k++;
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the number of rows of a rectangle: ");
@@ -107,5 +118,8 @@ public class Patterns {
         half_pyramid_with_numbers(a);
         System.out.println();
         half_inverted_pyramid_with_numbers(a);
+        System.out.println();
+        floyd_triangle(a);
+        System.out.println();
     }
 }
