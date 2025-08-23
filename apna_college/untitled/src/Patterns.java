1a8fed37e45972fcb44f00447439b2ab4cbaede1
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-08-23 19:35:26 +0530

    printing a hollow rectangle

--- apna_college/untitled/src/Patterns.java
+++ apna_college/untitled/src/Patterns.java
@@ -9,16 +9,30 @@ public class Patterns {
             System.out.println();
         }
     }
+
+    public static void hollow_rectangle(int n, int m) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (i == 1 || j == 1 || j == m || i == n) {
+                    System.out.print("*");
+                }
+                else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        // Printing a solid rectangle
         System.out.print("Enter the number of rows of a rectangle: ");
         int n = input.nextInt();
         System.out.print("Enter the number of columns of a rectangle: ");
         int m = input.nextInt();
         solid_rectangle(n,m);
-        //
+        hollow_rectangle(n,m);
 
     }
 }
