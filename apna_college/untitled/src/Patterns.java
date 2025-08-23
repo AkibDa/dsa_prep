e2700a8fa53fcd245ded75cdfb7cfcb1038cc091
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-08-23 20:40:40 +0530

    printing a number pyramid

--- apna_college/untitled/src/Patterns.java
+++ apna_college/untitled/src/Patterns.java
@@ -149,6 +149,18 @@ public class Patterns {
         }
     }
 
+    public static void number_pyramid(int n) {
+        for (int i = 1; i <= n; i++) {
+            for(int k = 1; k <= n - i; k++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i+" ");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the number of rows of a rectangle: ");
@@ -179,6 +191,8 @@ public class Patterns {
         butterfly_pattern(a);
         System.out.println();
         solid_rhombus(a);
+        System.out.println();
+        number_pyramid(a);
         System.out.println();
     }
 }
