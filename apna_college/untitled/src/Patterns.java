5eb0bfa41cb7dd15d0cad1fe51f0187d8c39102a
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-08-23 19:26:54 +0530

    printing a rectangle

--- apna_college/untitled/src/Patterns.java
+++ apna_college/untitled/src/Patterns.java
@@ -1,16 +1,24 @@
 import java.util.*;
 
 public class Patterns {
-    public static void solid_rectangle(int n) {
+    public static void solid_rectangle(int n, int m) {
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= i; j++) {
+            for (int j = 1; j <= m; j++) {
                 System.out.print("*");
             }
+            System.out.println();
         }
     }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+
+        // Printing a solid rectangle
+        System.out.print("Enter the number of rows of a rectangle: ");
         int n = input.nextInt();
-        solid_rectangle(n);
+        System.out.print("Enter the number of columns of a rectangle: ");
+        int m = input.nextInt();
+        solid_rectangle(n,m);
+        //
+
     }
 }
