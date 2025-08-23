7628ecf925661f7b4845ab008e6ae83a78dcb010
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-08-23 20:15:59 +0530

    printing a 0-1 triangle

--- apna_college/untitled/src/Patterns.java
+++ apna_college/untitled/src/Patterns.java
@@ -96,6 +96,20 @@ public class Patterns {
         }
     }
 
+    public static void zero_one_triangle(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                if ((i+j)%2 == 0) {
+                    System.out.print("1 ");
+                }
+                else {
+                    System.out.print("0 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter the number of rows of a rectangle: ");
@@ -120,6 +134,8 @@ public class Patterns {
         half_inverted_pyramid_with_numbers(a);
         System.out.println();
         floyd_triangle(a);
+        System.out.println();
+        zero_one_triangle(a);
         System.out.println();
     }
 }
