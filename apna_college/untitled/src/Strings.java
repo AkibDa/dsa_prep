52d09284809affb0cf83c4197069ed54b38a0d59
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-08-25 20:18:52 +0530

    learning about creating substring from string

--- apna_college/untitled/src/Strings.java
+++ apna_college/untitled/src/Strings.java
@@ -1,5 +1,3 @@
-
-
 public class Strings {
 
   public static void main(String[] args) {
@@ -13,14 +11,19 @@ public class Strings {
     System.out.println("Uppercase Full Name: " + fullName.toUpperCase());
     System.out.println("Lowercase Full Name: " + fullName.toLowerCase());
 
+    // Comparison
     String name1 = "Alice";
     String name2 = "Alice";
-
     if(name1.compareTo(name2) == 0) {
       System.out.println(name1 + " is equal to " + name2);
     } else {
       System.out.println(name1 + " is not equal to " + name2);
     }
+
+    // Substring
+    String str = "Hello, World!";
+    String substr = str.substring(7);
+    System.out.println("Substring: " + substr);
 
   }
 
