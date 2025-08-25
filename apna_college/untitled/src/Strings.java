c9ebcefe6bde148c18559e32848931d13c928fde
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-08-25 20:10:40 +0530

    using function to compare two strings

--- apna_college/untitled/src/Strings.java
+++ apna_college/untitled/src/Strings.java
@@ -13,6 +13,15 @@ public class Strings {
     System.out.println("Uppercase Full Name: " + fullName.toUpperCase());
     System.out.println("Lowercase Full Name: " + fullName.toLowerCase());
 
+    String name1 = "Alice";
+    String name2 = "Alice";
+
+    if(name1.compareTo(name2) == 0) {
+      System.out.println(name1 + " is equal to " + name2);
+    } else {
+      System.out.println(name1 + " is not equal to " + name2);
+    }
+
   }
 
 }
