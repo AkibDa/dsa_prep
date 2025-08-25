9a75922b062085c621c73d134fa2877d438eaba1
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-08-25 19:58:00 +0530

    using different functions with strings

--- apna_college/untitled/src/Strings.java
+++ apna_college/untitled/src/Strings.java
@@ -1,17 +1,18 @@
 
-import java.util.Scanner;
 
 public class Strings {
 
   public static void main(String[] args) {
 
-    Scanner sc = new Scanner(System.in);
-    String fullName;
-    System.out.print("Enter your full name: ");
-    fullName = sc.nextLine();
-    String sentence = "Welcome, " + fullName + ".";
-    System.out.println(sentence);
-    sc.close();
+    // Concatination
+    String firstName = "John";
+    String lastName = "Doe";
+    String fullName = firstName + " " + lastName;
+    System.out.println("Full Name: " + fullName); 
+    System.out.println("Length of Full Name: " + fullName.length());
+    System.out.println("Uppercase Full Name: " + fullName.toUpperCase());
+    System.out.println("Lowercase Full Name: " + fullName.toLowerCase());
+
   }
 
 }
