b6f1782498039b1057d45fae62de0084bbeb6c0f
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-08-29 20:22:26 +0530

    completed arraylist

--- apna_college/untitled/src/ArrayList.java
+++ apna_college/untitled/src/ArrayList.java
@@ -1,3 +1,5 @@
+import java.util.Collection;
+
 public class ArrayList {
   public static void main(String[] args) {
     java.util.ArrayList<String> list = new java.util.ArrayList<>();
@@ -7,5 +9,14 @@ public class ArrayList {
     list.remove("Hello");
     System.out.println(list);
     System.out.println("Size: " + list.size());
+    list.add(1, "element");
+    list.set(1, "hello");
+    System.out.println(list);
+    System.out.println("Contains 'World': " + list.contains("World"));
+    list.clear();
+    System.out.println("Is empty: " + list.isEmpty());
+    System.out.println("Final list: " + list);
+    Collection.sort(list);
+    System.out.println("Sorted list: " + list);
   }
 }
