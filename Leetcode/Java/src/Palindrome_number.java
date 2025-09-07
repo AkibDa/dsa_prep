400daad63ee3eb8bf7a0e6417d9e5308a6220c20
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-09-07 11:34:32 +0530

    done palindrome number check

---
+++ Leetcode/Java/src/Palindrome_number.java
@@ -0,0 +1,11 @@
+class Solution {
+  public boolean isPalindrome(int x) {
+      int m = x;
+      int ld = 0;
+      while(m>0){
+          ld = ld * 10 + m%10;
+          m = m/10;
+      }
+      return (ld == x);
+  }
+}
