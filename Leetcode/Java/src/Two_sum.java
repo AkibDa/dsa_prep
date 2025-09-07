971c1292e5e7346642ee4bdf9b7af663386141b0
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-09-07 11:29:26 +0530

    done two sum with the help of hashmap

--- Leetcode/Java/src/Two_sum.java
+++ Leetcode/Java/src/Two_sum.java
@@ -1,17 +1,16 @@
-class Solution {
-    public int[] Two_sum(int[] nums, int target) {
-        int i=0;
-        int j=nums.length-1;
-        while(i<j){
-            int sum=nums[i]+nums[j];
-            if(sum==target){
-                return new int[]{i+1,j+1};
-            } else {
-                i++;
-                j--;
-            }
-        }
-        return null;
+import java.util.HashMap;
+import java.util.Map;
 
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+            map.put(nums[i], i);
+        }
+        return new int[] {};
     }
 }
