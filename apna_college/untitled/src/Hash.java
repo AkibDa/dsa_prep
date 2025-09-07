a79b8a45675f3b5655517dc688a23b3df20e4836
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-09-07 09:56:56 +0530

    figuring hashset out

--- apna_college/untitled/src/Hash.java
+++ apna_college/untitled/src/Hash.java
@@ -1,3 +1,35 @@
+import java.util.HashSet;
+
 public class Hash {
   
+  public static void main(String[] args) {
+    //Creating a HashSet
+    HashSet<Integer> set = new HashSet<>();
+
+    //Adding elements to the HashSet
+    set.add(1);
+    set.add(2);
+    set.add(3);
+    set.add(1);
+
+    //Size
+    System.out.println("Size of HashSet: " + set.size());
+
+    //Search - contains
+    // if(set.contains(1)) {
+    //   System.out.println("Set contains 1");
+    // } else {
+    //   System.out.println("Set does not contain 1");
+    // }
+
+    //Remove an element
+    set.remove(2);
+    if(!set.contains(2)) {
+      System.out.println("Set does not contain 2");
+    }
+
+    //Print the HashSet
+    System.out.println("HashSet: " + set);
+  }
+
 }
