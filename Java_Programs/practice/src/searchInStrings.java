1d9fe504bb8f9194abe414227452fd1ae3de0a74
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-10-25 11:08:18 +0530

    searching using for each method

--- Java_Programs/practice/src/searchInStrings.java
+++ Java_Programs/practice/src/searchInStrings.java
@@ -3,7 +3,20 @@ public class searchInStrings {
         String name = "Akib";
         char target = 'u';
         System.out.println(search(name, target));
+        System.out.println(search2(name, target));
 
+    }
+
+    static  boolean search2(String str, char target){
+      if(str.length() == 0){
+        return false;
+      }
+      for(char ch : str.toCharArray()){
+        if(ch == target){
+          return true;
+        }
+      }
+      return false;
     }
 
     static boolean search(String str, char target){
