f53d837dadbd8b1ffe8bd92dcfa95f9843baba56
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-10-27 19:37:46 +0530

    searching in a sorted matrix

---
+++ Java_Programs/practice/src/sortedMatrix.java
@@ -0,0 +1,57 @@
+public class sortedMatrix {
+  public static void main(String[] args) {
+    int[][] matrix = {
+      {1, 2, 3, 4, 5},
+      {6, 7, 8, 9, 10},
+      {11, 12, 13, 14, 15},
+      {16, 17, 18, 19, 20},
+      {21, 22, 23, 24, 25}
+    };
+    int target = 19;
+    int[] result = search(matrix, target);
+    System.out.println("Target found at: [" + result[0] + ", " + result[1] + "]");
+  }
+
+  static int[] binarySearch(int[] matrix, int row, int cStart, int cEnd, int target){
+    while(cStart <= cEnd){
+      int mid = cStart + (cEnd - cStart) / 2;
+      if(matrix[mid] == target){
+        return new int[]{row, mid};
+      }
+      if(matrix[mid] < target){
+        cStart = mid + 1;
+      } else {
+        cEnd = mid - 1;
+      }
+    }
+    return new int[]{-1, -1};
+  }
+
+  static int[] search(int[][] matrix, int target){
+    int rows = matrix.length;
+    int cols = matrix[0].length;
+
+    if(rows == 1){
+      return binarySearch(matrix[0], 0, 0, cols - 1, target);
+    }
+
+    int rStart = 0;
+    int rEnd = rows - 1;
+    int cMid = cols / 2;
+
+    while(rStart < (rEnd - 1)){
+      int mid = rStart + (rEnd - rStart) / 2;
+      if(matrix[mid][cMid] == target){
+        return new int[]{mid, cMid};
+      }
+      if(matrix[mid][cMid] < target){
+        rStart = mid;
+      } else {
+        rEnd = mid;
+      }
+    }
+
+    return new int[]{-1, -1};
+  }
+
+}
