9af417422b87638312c38baa119b8a72c693c6ea
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-10-30 20:43:12 +0530

    Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive. There is only one repeated number in nums, return this repeated number.

---
+++ Leetcode/Java/src/FindDupNum.java
@@ -0,0 +1,16 @@
+public class FindDupNum {
+  public static int findDuplicate(int[] nums) {
+    while(nums[0] != nums[nums[0]]) {
+      int next = nums[nums[0]];
+      nums[nums[0]] = nums[0];
+      nums[0] = next;
+  }
+  return nums[0];
+  }
+
+  public static void main(String[] args) {
+    int[] nums = {1, 3, 4, 2, 2};
+    System.out.println("The duplicate number is: " + findDuplicate(nums)); // Output: 2
+  }
+
+}
