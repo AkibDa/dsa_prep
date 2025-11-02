e0e87c7328da38d73c10e95c9924adff7ddb55d1
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-11-02 18:09:59 +0530

    used the trick to solve a pattern

--- Java_Programs/practice/src/Patterns.java
+++ Java_Programs/practice/src/Patterns.java
@@ -1,19 +1,36 @@
 public class Patterns {
   public static void main(String[] args) {
-    pattern1();
+    pattern1(4);
+    System.out.println();
+    pattern2(5);
   }
 
-  static void pattern1(){
-    for(int i=1; i<=4; i++){  // Number of lines in the pattern = outer loop
+  static void pattern1(int n){
+    for(int i=1; i<=n; i++){  // Number of lines in the pattern = outer loop
       for(int j=1; j<=i; j++){  // For every row number , how many columns are there and what type of elemens are there in the columns
         System.out.print("* "); // What to print in the columns
       }
       System.out.println();
     }
   }
+  // Output:
+  // *
+  // * *
+  // * * *
+  // * * * *
+  
+  static void pattern2(int n){
+    for(int i=1;i<=n;i++){
+      for(int j=n;j>=i;j--){
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+  // Output:
+  // * * * * *
+  // * * * *
+  // * * *
+  // * *
+  // *
 }
-// Output:
-// *
-// * *
-// * * *
-// * * * *
