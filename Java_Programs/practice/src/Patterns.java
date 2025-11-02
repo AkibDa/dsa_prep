dd73873a2549b8ae46ee25b984abbaf1bdcee9fe
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2025-11-02 18:06:10 +0530

    step 3

--- Java_Programs/practice/src/Patterns.java
+++ Java_Programs/practice/src/Patterns.java
@@ -5,10 +5,15 @@ public class Patterns {
 
   static void pattern1(){
     for(int i=1; i<=4; i++){  // Number of lines in the pattern = outer loop
-      for(int j=1; j<=4; j++){  // For every row number , how many columns are there and what type of elemens are there in the columns
-        System.out.print("* ");
+      for(int j=1; j<=i; j++){  // For every row number , how many columns are there and what type of elemens are there in the columns
+        System.out.print("* "); // What to print in the columns
       }
       System.out.println();
     }
   }
 }
+// Output:
+// *
+// * *
+// * * *
+// * * * *
