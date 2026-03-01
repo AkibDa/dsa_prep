e71c3c80ee37dc8cf50a00b960ffa9f175def628
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2026-03-01 20:25:11 +0530

    uploaded all patterns from this topic

--- Patterns/Strings/three.py
+++ Patterns/Strings/three.py
@@ -4,17 +4,17 @@
 
 # Why: We need to group strings. The "key" for the group should be something common to all anagrams. Sorting the string (e.g., "eat" -> "aet") works as a perfect unique key.
 
-import collections
-
-
-def groupAnagrams(strs):
-  ans = collections.defaultdict(list)
-
+def groupAnagram(strs):
+  ans = {}
+  
   for s in strs:
-    # Create a key by sorting the string
-    key = tuple(sorted(s))
+    key = "".sorted(s)
+    
+    if key not in ans:
+      ans[key] = []
+        
     ans[key].append(s)
-
-  return ans.values()
+      
+  return list(ans.values())
 
 # Complexity: Time O(N ⚫ K log K) (where N is list length and K is max string length).
