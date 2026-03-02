89555dbe848e39071eada114b0eb70da83206065
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2026-03-02 20:34:19 +0530

    Write a function that reverses a string. The input string is given as an array of characters s.

--- Leetcode/Python/reverseString.py
+++ Leetcode/Python/reverseString.py
@@ -1,12 +1,12 @@
 def reverseString(s):
-    left = 0
-    right = len(s) - 1
+  left = 0
+  right = len(s) - 1
+  
+  while left < right:
+    s[left], s[right] = s[right], s[left]
     
-    while left < right:
-      s[left], s[right] = s[right], s[left]
-      
-      left += 1
-      right -= 1
-      
-    return s
+    left += 1
+    right -= 1
+    
+  return s
   
