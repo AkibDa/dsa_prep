a2c65d37989617022282b612a257972adf7967e0
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2026-06-23 15:34:48 +0530

    3699. Number of ZigZag Arrays I

---
+++ Leetcode/Python/zigZagArrays.py
@@ -0,0 +1,17 @@
+class Solution(object):
+  def zigZagArrays(self, n, l, r):
+    """
+    :type n: int
+    :type l: int
+    :type r: int
+    :rtype: int
+    """
+    MOD = 10 ** 9 + 7
+    r -= l
+    dp = [1] * (r + 1)
+    for _ in xrange(n - 1):
+      prefix = 0
+      for i in xrange(len(dp)):
+        dp[i], prefix = prefix, (prefix + dp[i]) % MOD
+      dp.reverse()
+    return (reduce(lambda accu, x: (accu + x) % MOD, dp, 0) * 2) % MOD
