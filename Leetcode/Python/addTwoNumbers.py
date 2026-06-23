57a4f62badc522cb226930768cfcd8d1efc2f118
Sk_Akib_Ahammed <ahammedskakib@gmail.com>
2026-06-23 11:09:21 +0530

    2. Add Two Numbers (faster runtime)

--- Leetcode/Python/addTwoNumbers.py
+++ Leetcode/Python/addTwoNumbers.py
@@ -15,20 +15,20 @@ class Solution(object):
         current = dummyHead
         carry = 0
 
-        while l1 is not None or l2 is not None or carry != 0:
+        while l1 or l2 or carry:
 
-          val1 = l1.val if l1 is not None else 0
-          val2 = l2.val if l2 is not None else 0
+          val1 = l1.val if l1 else 0
+          val2 = l2.val if l2 else 0
 
           total = val1 + val2 + carry
 
           carry = total // 10
-          newNode = ListNode(total % 10)
-          current.next = newNode
+          current.next = ListNode(total % 10)
+          current = current.next
 
-          if l1 is not None:
+          if l1:
             l1 = l1.next
-          if l2 is not None:
+          if l2:
             l2 = l2.next
 
         return dummyHead.next
